c7c4353fa04a20db2abb52454a7ca5c07d3716f6
chauhanparth210 <00chauhanparth@gmail.com>
2020-03-10 20:44:16 +0530

    add navbar

---
+++ src/components/NavBar/index.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import Logo from "../../asserts/pc-logo-2.png";
+import "./style.scss";
+
+const NavBar = () => {
+  return (
+    <div className="navbar">
+      <div className="navbar__logo">
+        <NavLink to="/" className="navbar__logo--link">
+          <img
+            className="navbar__logo--img"
+            src={Logo}
+            alt="Logo"
+            height="60"
+            width="60"
+          />
+        </NavLink>
+      </div>
+      <div className="navbar__container">
+        <ul className="navbar__items">
+          <li className="navbar__link">
+            <NavLink to="/create-blog" className="navbar__item">
+              Create Blog
+            </NavLink>
+          </li>
+          <li className="navbar__link">
+            <NavLink to="/blog" className="navbar__item">
+              Blog
+            </NavLink>
+          </li>
+          <li className="navbar__link">
+            <NavLink to="/login" className="navbar__item navbar__button">
+              Log in
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
---
+++ src/routers/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import NavBar from "../components/NavBar";
+
+function RouterPage() {
+  return (
+    <>
+      <NavBar />
+    </>
+  );
+}
+
+export default RouterPage;
