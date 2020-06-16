60eb4618ab5b6e3a3775b43308f022f51dfdc858
chauhanparth210 <00chauhanparth@gmail.com>
2020-06-16 09:28:31 +0530

    make it better

--- src/components/Contest/socket.js
+++ src/components/Contest/socket.js
@@ -1,5 +1,5 @@
-import io from 'socket.io-client'
+import io from "socket.io-client";
 
-const socket = io.connect('http://127.0.0.1:5000')
+const socket = io.connect("https://pc-daiict.herokuapp.com");
 
-export default socket
+export default socket;
